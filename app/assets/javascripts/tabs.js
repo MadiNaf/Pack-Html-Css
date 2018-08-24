cc11247ef3bf143c1603b1e7e0afdaf970124166
MadiNaf <madiats2@gmail.com>
2018-08-25 00:16:11 +0200

    pack-html-css

---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,23 @@
+//html doc 
+$(document).ready(function(){
+	$(".card").hide();
+    $(".html-doc").click(function(){
+        $(".card-html-doc").toggle();
+    });
+});
+
+//html video
+$(document).ready(function(){
+    $(".html-video").click(function(){
+        $(".card-html-vdo").toggle();
+    });
+});
+//html autre
+$(document).ready(function(){
+    $(".html-autre").click(function(){
+        $(".card-html-otre").toggle();
+    });
+});
+//css doc
+//css video
+//css autre
